1ef2adabc59ff702959b431ad678391f03eab0f2
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-04-04 02:34:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 # Add  code here!
-def prime? (n)
-  if n <= 1
+def prime? (int)
+  if int <= 1
         false
-    elsif n == 2
+    elsif int == 2
        true
     else
-        (2..n/2).none? { |i| n % i == 0}
+        (2..int/2).none? { |i| int % i == 0}
     end
 end
